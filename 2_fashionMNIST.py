6d0b46ad2c35471707cfeefdb76d7f6844262fe4
Kian Yang Lee <ken.lee.kianyang@gmail.com>
2020-07-13 12:14:24 +0800

    Implemented optional exercise 1 to check on effect of adding neurons to NN performance and training time

--- 2_fashionMNIST.py
+++ 2_fashionMNIST.py
@@ -50,10 +50,44 @@ model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
 # model building
-model.compile(loss = "sparse_categorical_crossentropy",
+model.compile(loss = 'sparse_categorical_crossentropy',
                 optimizer = tf.optimizers.Adam(),
                 metrics=['accuracy'])
 model.fit(training_images, training_labels, epochs=5)
 
 # model evaluation
 model.evaluate(test_images, test_labels)
+
+# perform prediction - Q: is the prediction accurate?
+classifications = model.predict(test_images)
+print(classifications[0])
+print(test_labels[0])
+
+# Optional Exercise 1
+# Please uncomment only these parts and comment out the rest
+# in order to run
+# Q: does increase in neurons affect model accuracy and training time?
+# import tensorflow as tf
+
+# mnist = tf.keras.datasets.mnist
+
+# (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+# training_images = training_images/255.0
+# test_images = test_images/255.0
+
+# model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+#                                     tf.keras.layers.Dense(1024, activation=tf.nn.relu),
+#                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+# model.compile(optimizer='adam',
+#                loss='sparse_categorical_crossentropy')
+
+# model.fit(training_images, training_labels, epochs=5)
+
+# model.evaluate(test_images, test_labels)
+
+# classifications = model.predict(test_images)
+
+# print(classifications[0])
+# print(test_labels[0])
