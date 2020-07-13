d7bee5974bf285bd0253d3a260c690a655966c2c
Kian Yang Lee <ken.lee.kianyang@gmail.com>
2020-07-13 11:49:12 +0800

    Implemented NN to classify apparels using Fashion MNIST dataset

---
+++ 2_fashionMNIST.py
@@ -0,0 +1,59 @@
+#!usr/bin/env python3
+
+"""
+In the previous exercise you saw how to create a neural network
+ that figured out the problem you were trying to solve.
+  This gave an explicit example of learned behavior.
+   Of course, in that instance, it was a bit of overkill because 
+   it would have been easier to write the function Y=2x-1 directly,
+    instead of bothering with using Machine Learning to learn
+     the relationship between X and Y for a fixed set of values
+     , and extending that for all values.
+
+But what about a scenario where writing rules like that is much
+ more difficult -- for example a computer vision problem?
+  Let's take a look at a scenario where we can recognize different
+   items of clothing, trained from a dataset containing 10 different types.
+"""
+
+# import package
+import tensorflow as tf
+print(tf.__version__)
+import numpy as np
+import matplotlib.pyplot as plt
+
+# accessing dataset directly from tf.keras datasets API
+mnist = tf.keras.datasets.fashion_mnist
+
+# loading into train and test set
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+# The number of characters per line for the purpose of inserting line breaks (default 75).
+np.set_printoptions(linewidth=200)
+
+# displaying first image, printing its labels and image pixel in array form
+# you can change the index to view other images
+plt.imshow(training_images[0])
+print(training_labels[0])
+print(training_images[0])
+
+# normalization
+training_images = training_images/255.0
+test_images = test_images/255.0
+
+# model configuration
+# Sequential = defines a sequence of layers in the NN
+# Flatten = turns t into a 1D array
+# Dense = add a layer of interconnected neurons
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+# model building
+model.compile(loss = "sparse_categorical_crossentropy",
+                optimizer = tf.optimizers.Adam(),
+                metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=5)
+
+# model evaluation
+model.evaluate(test_images, test_labels)
